6a39979b1ab8b7b64985865834dc1f38d0ee6488
Duncan Champney <DuncanC@aol.com>
2015-04-16 15:35:28 -0400

    Initial commit

---
+++ DatesInSwift/DateExtensions.swift
@@ -0,0 +1,84 @@
+//
+//  DateExtensions.swift
+//  DatesInSwift
+//
+//  Created by Duncan Champney on 4/16/15.
+//  Copyright (c) 2015 Duncan Champney. All rights reserved.
+//
+
+public typealias mdyTuple = (month: Int, day: Int, year: Int)
+
+import UIKit
+
+/**
+  This function makes it possible to use mdyTuple constants or variables in the cases
+  of switch statements.
+*/
+func ~=(a: mdyTuple, b: mdyTuple) -> Bool {
+  return a.month ~= b.month && a.year ~= b.year && a.day ~= b.day
+}
+
+/**
+  This function makes it possible to compar mdyTuple objects using ==
+*/
+public func ==(lhs: mdyTuple, rhs: mdyTuple) -> Bool
+{
+  return lhs.month == rhs.month &&
+  lhs.day == rhs.day &&
+  lhs.year == rhs.year
+}
+
+//-------------------------------------------------------------
+//Tell the system that NSDates can be compared with ==, >, >=, <, and <= operators
+extension NSDate: Equatable {}
+extension NSDate: Comparable {}
+
+//-------------------------------------------------------------
+//Define the global operators for the
+//Equatable and Comparable protocols for comparing NSDates
+
+public func ==(lhs: NSDate, rhs: NSDate) -> Bool
+{
+  return lhs.timeIntervalSince1970 == rhs.timeIntervalSince1970
+}
+
+public func <(lhs: NSDate, rhs: NSDate) -> Bool
+{
+  return lhs.timeIntervalSince1970 < rhs.timeIntervalSince1970
+}
+public func >(lhs: NSDate, rhs: NSDate) -> Bool
+{
+  return lhs.timeIntervalSince1970 > rhs.timeIntervalSince1970
+}
+public func <=(lhs: NSDate, rhs: NSDate) -> Bool
+{
+  return lhs.timeIntervalSince1970 <= rhs.timeIntervalSince1970
+}
+public func >=(lhs: NSDate, rhs: NSDate) -> Bool
+{
+  return lhs.timeIntervalSince1970 >= rhs.timeIntervalSince1970
+}
+//-------------------------------------------------------------
+
+extension NSDate
+{
+  /**
+  This function returns a tuple containing the month, day and year of the receiver
+  
+  :returns: typealias mdyTuple = (month: Int, day: Int, year: Int)
+*/
+  func mdy() -> mdyTuple
+  {
+    let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+    
+    //get the month/day/year componentsfor today's date.
+    let date_components = calendar.components(
+      NSCalendarUnit.CalendarUnitYear |
+        NSCalendarUnit.CalendarUnitMonth |
+        NSCalendarUnit.CalendarUnitDay,
+      fromDate: self)
+    
+
+    return (date_components.month, date_components.day, date_components.year)
+  }
+}
--- DatesInSwift/ViewController.swift
+++ DatesInSwift/ViewController.swift
@@ -8,18 +8,116 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+func possessiveNumber(aNumber: Int) -> String
+{
+  let suffix: String
+  switch (aNumber % 10)
+  {
+  case 0, 4...9:
+    suffix = "th"
+  case 1:
+    suffix = "st"
+  case 2:
+    suffix = "nd"
+  case 3:
+    suffix = "rd"
+  default:
+    suffix = ""
+  }
+  return "\(aNumber)\(suffix)"
+}
+class ViewController: UIViewController
+{
+  var message: String = ""
+    {
+    didSet
+    {
+      messageLabel.text = message
+    }
+  }
+  @IBOutlet weak var messageLabel: UILabel!
+  @IBOutlet weak var datePicker: UIDatePicker!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
+  override func didReceiveMemoryWarning()
+  {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
 
 
+  @IBAction func pickerValueChanged(sender: AnyObject)
+  {
+    message = ""
+  }
+  
+
+  let duncanCsBirthday = (month: 5, day: 18, year: 1963)
+  let markCsBirthday = (month: 4, day: 17, year: 1959)
+  let daveCsBirthay = (month: 1, day: 2, year: 1937)
+  let colinCsBirtday = (month: 12, day: 14, year: 1994)
+  
+  @IBAction func handleOkButton(sender: AnyObject)
+  {
+    let prefix: String
+    
+    //I compare the mdy tuples for the dates so I can ignore the time and just compare dates.
+    if datePicker.date.mdy() == NSDate().mdy()
+    {
+      prefix = "is"
+    }
+    else if datePicker.date > NSDate()
+    {
+      prefix = "will be"
+    }
+    else
+    {
+      prefix = "was"
+    }
+    
+    let theMDY = datePicker.date.mdy()
+    let date = NSString(format: "%02d/%02d/%04d", theMDY.month, theMDY.day, theMDY.year)
+    switch theMDY
+    {
+    case (3, 15, _):
+      message = "Beware the Ides of March"
+      
+    case (daveCsBirthay):
+      message = "\(date) \(prefix) the day Dave Champney was born"
+      
+    case (daveCsBirthay.month, daveCsBirthay.day, let year) where year > daveCsBirthay.year:
+      message = "\(date) \(prefix) Dave Champney's \(possessiveNumber(theMDY.year - daveCsBirthay.year)) birthday"
+      
+    case (markCsBirthday):
+      message = "\(date) \(prefix) the day Mark Champney was born"
+      
+    case (4, 17, let year) where year > markCsBirthday.year:
+      message = "\(date) \(prefix) Mark Champney's \(possessiveNumber(year - markCsBirthday.year)) birthday"
+      
+    case (duncanCsBirthday):
+      message = "\(date) \(prefix) the day Duncan Champney was born"
+      
+    case (duncanCsBirthday.month, duncanCsBirthday.day, let year) where year > duncanCsBirthday.year:
+      message = "\(date) \(prefix) Duncan Champney's \(possessiveNumber(year - duncanCsBirthday.year)) birthday"
+      
+    case (colinCsBirtday):
+      message = "\(date) \(prefix) the day Colin Champney was born"
+      
+    case (colinCsBirtday.month, colinCsBirtday.day, let year) where year > colinCsBirtday.year:
+      message = "\(date) \(prefix) Colin Champney's \(possessiveNumber(year - colinCsBirtday.year)) birthday"
+      
+    case (5, 1...15, let year):
+      message = "\(date) \(prefix) in the first half of May, \(year)"
+      
+    case (5, 16...31, let year):
+      message = "\(date) \(prefix) in the second half of May, \(year)"
+      
+    default:
+      message = "The selected date \(prefix) \(date)"
+    }
+  }
 }
 
